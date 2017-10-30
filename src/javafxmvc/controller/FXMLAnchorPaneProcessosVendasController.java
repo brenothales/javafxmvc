ad98f2a3f52f346d641a9c56e76b033d2add3a3b
Breno Thales <breno.thales@gmail.com>
2017-10-29 22:12:10 -0200

    Gerando as variáveis e passando os tipo dela

---
+++ src/javafxmvc/controller/FXMLAnchorPaneProcessosVendasController.java
@@ -0,0 +1,28 @@
+//    Tem que declarar dos tipo de lista venda
+    @FXML
+    private TableView<Venda> tableViewVendas;
+//    Tem que declarar dos tipo de lista venda
+    @FXML
+    private TableColumn<Venda, Integer> tableColumnVendaCodigo;
+//    Tem que declarar dos tipo de lista venda    
+    @FXML
+    private TableColumn<Venda, LocaleData> tableColumnVendaData;
+//    Tem que declarar dos tipo de lista venda    
+    @FXML
+    private TableColumn<Venda, Venda> tableColumnVendaCliente;
+    @FXML
+    private Label labelVendaCodigo;
+    @FXML
+    private Label labelVendaData;
+    @FXML
+    private Label labelVendaValor;
+    @FXML
+    private Label labelVendaPago;
+    @FXML
+    private Label labelVendaCliente;
+    @FXML
+    private Button buttonInserir;
+    @FXML
+    private Button buttonAlterar;
+    @FXML
+    private Button buttonRemover;
