a8f737bd5eefeeb42cf8b2e5bec3ef7d4cc46a30
Breno Thales <breno.thales@gmail.com>
2017-10-29 22:14:25 -0200

    Fazendo os importes necessário para rodar a nossa aplicação

--- src/javafxmvc/controller/FXMLAnchorPaneProcessosVendasController.java
+++ src/javafxmvc/controller/FXMLAnchorPaneProcessosVendasController.java
@@ -1,3 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxmvc.controller;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafxmvc.model.dao.ItemDeVendaDAO;
+import javafxmvc.model.dao.ProdutoDAO;
+import javafxmvc.model.dao.VendaDAO;
+import javafxmvc.model.database.Database;
+import javafxmvc.model.database.DatabaseFactory;
+import javafxmvc.model.domain.Venda;
+import sun.util.resources.LocaleData;
+
+/**
+ * FXML Controller class
+ *
+ * @author brenothales
+ */
+public class FXMLAnchorPaneProcessosVendasController implements Initializable {
 //    Tem que declarar dos tipo de lista venda
     @FXML
     private TableView<Venda> tableViewVendas;
@@ -39,3 +71,12 @@
     private final ProdutoDAO produtoDAO = new ProdutoDAO();
     
 
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
